d258c72b4325a018055755145228a06c37ac245b
Max Rozdobudko <max.rozdobudko@gmail.com>
2026-02-19 11:45:28 +0200

    docs: updates platforms list

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [
         .iOS(.v18),
         .macOS(.v15),
+        .watchOS(.v11),
+        .tvOS(.v18),
+        .visionOS(.v2),
     ],
     products: [
         .library(
